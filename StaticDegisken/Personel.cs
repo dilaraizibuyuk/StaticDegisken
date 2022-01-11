4a9499d69437a5cf661039723ecaad03488fda7d
Dilara <74327986+dilaraizibuyuk@users.noreply.github.com>
2022-01-11 16:18:18 +0300

    Proje dosyası ekle.

---
+++ StaticDegisken/Personel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDegisken
+{
+    class Personel
+    {
+        /////////1.yöntem:değişken tanımlama//////////
+        //public int personelID;
+        //public string Ad;
+        //public string Soyad;
+        //public static int maas; //static değişken
+
+
+        ////////2.yöntem:prop ile///////////
+        public int personelID { get; set; }
+        public string Ad { get; set; }
+
+        public string Soyad { get; set; }
+
+        public static int Maas { get; set; } //static değişken
+
+
+    }
+}
---
+++ StaticDegisken/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDegisken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /////1.yöntem/////
+            //Personel P2 = new Personel();
+            //P2.personelID = 1;
+            //P2.Ad = "Dilara";
+            //P2.Soyad = "İzibüyük";
+            //Personel.maas = 50000;
+            //Console.WriteLine("Personel ID'si:"+P2.personelID);
+            //Console.WriteLine("Personel Adı:" + P2.Ad);
+            //Console.WriteLine("Personel Soyad:" + P2.Soyad);
+            //Console.WriteLine("Personel Maaş:" + Personel.maas);
+
+
+            ////////2.yöntem:prop ile///////////
+            Personel P2 = new Personel();
+            P2.personelID = 1;
+            P2.Ad = "Dilara";
+            P2.Soyad = "İzibüyük";
+            Personel.Maas = 50000;
+            Console.WriteLine("Personel ID'si:" + P2.personelID);
+            Console.WriteLine("Personel Adı:" + P2.Ad);
+            Console.WriteLine("Personel Soyad:" + P2.Soyad);
+            Console.WriteLine("Personel Maaş:" + Personel.Maas);
+
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
